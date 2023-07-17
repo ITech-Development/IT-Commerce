6d55c87734b0883b2c8f771b93246b0a2870d550
evans2101 <evans.moris21@gmail.com>
2023-07-17 16:52:36 +0700

    pulling

--- server/models/adminseller.js
+++ server/models/adminseller.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      AdminSeller.hasMany(models.Order, {
+      AdminSeller.hasMany(models.User, {
         foreignKey: 'adminSellerId',
         as: 'adminSeller'
       })
