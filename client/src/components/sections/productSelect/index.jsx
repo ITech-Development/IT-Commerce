a87d0ea8334b2ac58d455f26bb6ed611d74e0b3a
evans2101 <evans.moris21@gmail.com>
2023-07-18 15:35:12 +0700

    pulling

--- client/src/components/sections/productSelect/index.jsx
+++ client/src/components/sections/productSelect/index.jsx
@@ -1,20 +1,33 @@
 import React from "react";
 import "./productSelect.css";
+import { Link } from "react-router-dom";
 
 export default function index() {
   return (
-    <div className="views" >
-
-        <h1 style={{ color: "#010F52", maxWidth: "350px" }}>
-          Belanja <br />
-          Sekarang Juga
-        </h1>
-        <h4 style={{ color: "#010F52" }}>
-          Ingin mengakses produk yang tepat dari kategori yang disesuaikan?
-        </h4>
+    <div className="views">
+      <h1 style={{ color: "#010F52", maxWidth: "350px" }}>
+        Belanja <br />
+        Sekarang Juga
+      </h1>
+      <h4 style={{ color: "#010F52" }}>
+        Ingin mengakses produk yang tepat dari kategori yang disesuaikan?
+      </h4>
 
       <div>
-        <button style={{backgroundColor: '#243A73', color: 'white', padding: '15px 25px', border: '1px solid #243A73', borderRadius: '5px', cursor: 'pointer'}}>Belanja & Dapatkan Promo Produk </button>
+        <button
+          style={{
+            backgroundColor: "#243A73",
+            color: "white",
+            padding: "15px 25px",
+            border: "1px solid #243A73",
+            borderRadius: "5px",
+            cursor: "pointer",
+          }}
+        >
+          <Link to='/productlist'>
+          Belanja & Dapatkan Promo Produk{" "}
+          </Link>
+        </button>
       </div>
     </div>
   );
