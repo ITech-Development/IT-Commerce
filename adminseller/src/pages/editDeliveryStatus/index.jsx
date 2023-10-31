741dd8c8d72fa03c9e1d1b4267797329fac77638
evans2101 <evans.moris21@gmail.com>
2023-10-31 09:35:07 +0700

    update ui orderStatus in adminSeller

--- adminseller/src/pages/editDeliveryStatus/index.jsx
+++ adminseller/src/pages/editDeliveryStatus/index.jsx
@@ -1,131 +1,137 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
-import styled from 'styled-components';
-
+import React, { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import styled from "styled-components";
 const Container = styled.div`
-  width: 300px;
-  margin: auto;
-  padding: 50px;
-  background-color: #fff;
+  max-width: 400px;
+  margin: 0 auto;
+  padding: 20px;
   border: 1px solid #ccc;
   border-radius: 5px;
+  background-color: #fff;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
 `;
 
-const Title = styled.h2`
+const Title = styled.h1`
   font-size: 24px;
-  color: #333;
-  margin: auto;
+  margin-bottom: 20px;
   text-align: center;
-  padding-bottom: 20px;
 `;
 
 const Form = styled.form`
   display: flex;
   flex-direction: column;
-  align-items: center;
+  // align-items: center;
 `;
 
+const Label = styled.label`
+  margin-bottom: 10px;
+`;
 
 const Select = styled.select`
-  width: 220px;
+  width: 100%;
   padding: 10px;
-  font-size: 16px;
   border: 1px solid #ccc;
   border-radius: 5px;
-  margin-bottom: 30px;
+  outline: none;
+`;
+
+const Input = styled.input`
+  width: 94.5%;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  outline: none;
 `;
 
 const Button = styled.button`
-  width: 100%;
-  padding: 10px;
-  background-color: #007bff;
+  padding: 10px 20px;
+  background-color: #007BFF;
   color: #fff;
-  font-size: 18px;
   border: none;
   border-radius: 5px;
   cursor: pointer;
+  font-size: 16px;
+  margin-top: 20px;
+  transition: background-color 0.3s; /* Menambahkan efek transisi pada warna latar belakang */
+  
+  &:hover {
+    background-color: #0056b3; /* Warna saat tombol di-hover */
+  }
 `;
-
 function EditCheckout() {
-    const { id } = useParams();
-    const navigate = useNavigate();
-    const [deliveryStatus, setDeliveryStatus] = useState('');
-    const [trackingNumber, setTrackingNumber] = useState('');
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const [deliveryStatus, setDeliveryStatus] = useState("");
+  const [trackingNumber, setTrackingNumber] = useState("");
 
-    useEffect(() => {
-        // Fetch data as before
-        fetch(`https://indoteknikserver-732012365989.herokuapp.com/checkouts/${id}`)
-            .then(response => response.json())
-            .then(data => {
-                setDeliveryStatus(data.deliveryStatus);
-                setTrackingNumber(data.trackingNumber);
-            });
-    }, [id]);
+  useEffect(() => {
+    // Fetch data as before
+    fetch(`https://indoteknikserver-732012365989.herokuapp.com/checkouts/${id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setDeliveryStatus(data.deliveryStatus);
+        setTrackingNumber(data.trackingNumber);
+      });
+  }, [id]);
 
   const handleDeliveryStatusChange = (event) => {
     setDeliveryStatus(event.target.value);
   };
 
-    const handleTrackingNumberChange = (event) => {
-        setTrackingNumber(event.target.value);
-    };
+  const handleTrackingNumberChange = (event) => {
+    setTrackingNumber(event.target.value);
+  };
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        // Send the HTTP request as before
-        fetch(`https://indoteknikserver-732012365989.herokuapp.com/checkouts/${id}`, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json',
-                access_token: localStorage.getItem('access_token')
-            },
-            body: JSON.stringify({ deliveryStatus, trackingNumber }),
-        })
-            .then(response => {
-                if (response.status === 201) {
-                    alert('ok');
-                    navigate('/order-list');
-                } else {
-                    alert('error');
-                }
-            });
-    };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Send the HTTP request as before
+    fetch(
+      `https://indoteknikserver-732012365989.herokuapp.com/checkouts/${id}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          access_token: localStorage.getItem("access_token"),
+        },
+        body: JSON.stringify({ deliveryStatus, trackingNumber }),
+      }
+    ).then((response) => {
+      if (response.status === 201) {
+        alert("ok");
+        navigate("/order-list");
+      } else {
+        alert("error");
+      }
+    });
+  };
 
   return (
-    <div style={{marginTop: '200px'}}>
-
-    <Container>
-      <Title>Edit Status Pesanan</Title>
+    <div style={{ marginTop: "200px" }}>
+       <Container>
+      <Title>Status Pesanan</Title>
       <Form onSubmit={handleSubmit}>
         <div>
-            <h2>Edit Checkout</h2>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="deliveryStatus">Delivery Status:</label>
-                    <Select
-                        id="deliveryStatus"
-                        value={deliveryStatus}
-                        onChange={handleDeliveryStatusChange}
-                        required
-                    >
-                        <option value="Sedang dikemas">Sedang dikemas</option>
-                        <option value="Dikirim">Dikirim</option>
-                        {/* Add more options as needed */}
-                    </Select>
-                </div>
-                <div>
-                    <label htmlFor="trackingNumber">Tracking Number:</label>
-                    <input
-                        type="text"
-                        id="trackingNumber"
-                        value={trackingNumber}
-                        onChange={handleTrackingNumberChange}
-                        required
-                    />
-                </div>
-                <button type="submit">Update</button>
-            </form>
+          <Label htmlFor="deliveryStatus">Delivery Status:</Label>
+          <Select
+            id="deliveryStatus"
+            value={deliveryStatus}
+            onChange={handleDeliveryStatusChange}
+            required
+          >
+            <option value="Sedang dikemas">Sedang dikemas</option>
+            <option value="Dikirim">Dikirim</option>
+            {/* Add more options as needed */}
+          </Select>
+        </div>
+        <div style={{ paddingTop: '15px'}}>
+          <Label htmlFor="trackingNumber">Tracking Number:</Label>
+          <Input
+            type="text"
+            id="trackingNumber"
+            value={trackingNumber}
+            onChange={handleTrackingNumberChange}
+            required
+          />
         </div>
         <Button type="submit">Update</Button>
       </Form>
